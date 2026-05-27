613d24d8333cecee0e63ff9dff269ac5f7707c81
Pavuc <anim10112005@gmail.ru>
2026-05-27 20:53:22 +0300

    Module 7

--- api_v1/ursl.py
+++ api_v1/ursl.py
@@ -1,6 +1,12 @@
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .views import AuthorViewSet, BookViewSet, CommentViewSet
 
-from django.urls import path
-
+router = DefaultRouter()
+router.register(r'authors', AuthorViewSet, basename='author')
+router.register(r'books', BookViewSet, basename='book')
+router.register(r'comments', CommentViewSet, basename='comment')
 
 urlpatterns = [
+    path('', include(router.urls)),
 ]
