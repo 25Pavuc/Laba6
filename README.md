ab549d434ff396980147a1f41ae1be91c39f9296
Pavuc <anim10112005@gmail.ru>
2026-05-27 20:08:20 +0300

    Module 4

--- README.md
+++ README.md
@@ -1,4 +1,28 @@
 # LABA 6 MADE BY FEDOSEEV PAVEL
-# Module 3
+# Module 4
 
-Была создана и подключена папка api_v1 в которой содержится информация о приложении
+Была создана база данных библиотеки
+
+class Author(models.Model):
+    name = models.CharField(max_length=255)
+    birth_date = models.DateField(null=True, blank=True)
+    bio = models.TextField(blank=True)
+    genres = models.ManyToManyField(Genre, related_name='books', blank=True)
+
+class Book(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField(blank=True)
+    published_date = models.DateField(null=True, blank=True)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE, related_name='books')
+    created_at = models.DateTimeField(auto_now_add=True)
+
+class Genre(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+
+    def __str__(self):
+        return self.name
+
+class Comment(models.Model):
+    book = models.ForeignKey(Book, on_delete=models.CASCADE, related_name='comments')
+    text = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
--- api_v1/models.py
+++ api_v1/models.py
@@ -1,3 +1,35 @@
 from django.db import models
 
-# Create your models here.
+class Author(models.Model):
+    name = models.CharField(max_length=255)
+    birth_date = models.DateField(null=True, blank=True)
+    bio = models.TextField(blank=True)
+
+    def __str__(self):
+        return self.name
+    
+class Genre(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+
+    def __str__(self):
+        return self.name
+    
+class Book(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField(blank=True)
+    published_date = models.DateField(null=True, blank=True)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE, related_name='books')
+    created_at = models.DateTimeField(auto_now_add=True)
+    genres = models.ManyToManyField(Genre, related_name='books', blank=True)
+
+    def __str__(self):
+        return self.title
+
+
+class Comment(models.Model):
+    book = models.ForeignKey(Book, on_delete=models.CASCADE, related_name='comments')
+    text = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'Comment on {self.book.title}'
